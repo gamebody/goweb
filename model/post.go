1c231b29cc11430b05ab131d5e3840119fc63ff7
liuyun <liuyun@nexbrio.com>
2019-08-26 18:51:15 +0800

    列表

--- model/post.go
+++ model/post.go
@@ -2,16 +2,18 @@ package model
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/gamebody/goweb/initdb"
 )
 
 // Post 用户相关字段
 type Post struct {
-	Author  int64  `form:"author"`
-	Title   string `form:"title"`
-	Content string `form:"content"`
-	PV      int    `form:"pv"`
+	Author     int64  `form:"author"`
+	Title      string `form:"title"`
+	Content    string `form:"content"`
+	PV         int    `form:"pv"`
+	CreateTime int    `form:"create_time"`
 }
 
 // Save 储存用户
@@ -29,7 +31,7 @@ func (p *Post) Save() error {
 func GetPosts(author int64) []Post {
 	var posts []Post
 
-	stmtOut, err := initdb.Db.Prepare("SELECT author,title,content,pv FROM blog.post WHERE author = ?")
+	stmtOut, err := initdb.Db.Prepare("SELECT author,title,content,pv,create_time FROM blog.post WHERE author = ?")
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
@@ -42,7 +44,9 @@ func GetPosts(author int64) []Post {
 
 	for rows.Next() {
 		post := &Post{}
-		rows.Scan(&post.Author, &post.Title, &post.Content, &post.PV)
+		rows.Scan(&post.Author, &post.Title, &post.Content, &post.PV, &post.CreateTime)
+
+		fmt.Println(*post)
 
 		posts = append(posts, *post)
 	}
